1650bdc22303144216f0bf0dab67db216c34a7ec
Todd Reichert <toddreichert@mac.com>
2025-02-20 13:16:13 -0600

    added division test

--- rpnTest.java
+++ rpnTest.java
@@ -21,4 +21,11 @@ public class rpnTest {
         String[] args = {"3","4","*"};
         assertEquals("12", rpn.main(args));
     }
+
+    @Test
+    public void divisionTest() {
+        rpn rpn = new rpn();
+        String[] args = {"12","4","/"};
+        assertEquals("3", rpn.main(args));
+    }
 }
